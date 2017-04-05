2c986815cc965418943f589d601b123dccd206a1
jonsnow123kappa <johnny.uncaught@gmail.com>
2017-04-05 21:24:51 +0300

    Redesigned script logic, improved perfomance

--- main.py
+++ main.py
@@ -1,74 +1,59 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Author: jonsnow123kappa
-# e-mail: johnny.uncaught@gmail.com
-
-import vk
-import urllib.request
+from client import User
 from multiprocessing import Pool
 import os
-import getpass
+import requests
+import shutil
+import functools
 import time
-
-start_time = time.time()
-
-
-def login(app_id='5606767'):
-    '''Login and initiate VK session.'''
-
-    login = str(input("Enter your login: "))
-    password = str(getpass.getpass("Enter your password: "))
-    session = vk.AuthSession(app_id, login, password, scope='photos')
-    return vk.API(session)
+import getpass
 
 
-def get_saved_photos():
-    '''Get all of the source links for photos in "Saved" album.'''
+def create_folder(credentials):
+    '''Create a folder to store photos named after your VK
+    first and last name.'''
 
-    saved_photos = vk_api.photos.get(album_id='saved')
-
-    photos_urls = []
-    for photo in saved_photos:
-        for key, value in photo.items():
-            if (key == 'src_big'):
-                photos_urls.append(value)
-    print("You've got {} saved photos!".format(len(photos_urls)))
-    return photos_urls
-
-
-def make_folder():
-    '''Create a folder called after your first and last name'''
-
-    folder_name = vk_api.users.get()[0]['first_name'] +\
-        "_" + vk_api.users.get()[0]['last_name']
+    folder_name = "_".join(credentials)
     if not os.path.exists(folder_name):
         os.makedirs(folder_name, exist_ok=True)
-    print("Directory {} is already created!".format(folder_name))
-    return str(folder_name)
+    print("Directory {} is created!".format(folder_name))
+    return folder_name
 
 
-def photos_downloader(url):
-    '''Download all the photos in newly-created directory
-     named after your VK credentials.'''
+def photos_downloader(url, folder_name):
+    '''Download all the photos into folder_name'''
 
-    file_name = str(url.split('/')[-1])
-    u = urllib.request.urlopen(url)
-    with open(os.path.join(folder_name, file_name), 'wb') as f:
-        f.write(u.read())
-        print("{} downloaded.".format(file_name))
+    file_name = str(url.split("/")[-1])
+    s = requests.Session()
+    r = s.get(url, stream=True)
+    if r.status_code == 200:
+        with open(os.path.join(folder_name, file_name), "wb") as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+            print("{} downloaded.".format(file_name))
 
 
 if __name__ == '__main__':
-
+    user_login = input("Enter your login: ")
+    user_password = getpass.getpass("Enter your password: ")
     try:
-        vk_api = login()
-    except vk.exceptions.VkAuthError as e:
-        print(e)
+        user = User(login=user_login, password=user_password)
+        vk_api = user.auth()
+        print("Authorization successful.")
+    except Exception:
+        print("Your input data seems to be wrong. Please try again!")
     else:
-        folder_name = make_folder()
-        p = Pool()
-        urls = [image for image in get_saved_photos()]
-        p.map(photos_downloader, urls)
-    finally:
-        print("---Done in %s seconds ---" % (time.time() - start_time))
+        try:
+            creds = user.get_credentials(vk_api)
+            folder = create_folder(creds)
+
+            p = Pool()
+            urls = [image for image in user.get_photos(vk_api)]
+            start_time = time.time()
+            p.map(functools.partial(photos_downloader,
+                                    folder_name=folder), urls)
+        except Exception as e:
+            print(e)
+        except KeyboardInterrupt:
+            print("Keyboard interrupt detected!")
+        finally:
+            print("---Done in {} seconds ---".format(time.time() - start_time))
